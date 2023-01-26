fde05be71a10fff19f00aa81c40049a0703ff8f8
callmetaufeeq <callmetaufeeq@gmail.com>
2023-01-26 11:31:42 +0530

    added one line of code

--- src/com/tw/methodOverloading/MainClass.java
+++ src/com/tw/methodOverloading/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		MethodOverriding mo=new MethodOverriding();
 		System.out.println(mo.add(2, 2));
+		System.out.println(mo.add(0, 0, 0));
 	}
 
 }
