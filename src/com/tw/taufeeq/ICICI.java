3a0aa0c602e2f87d2bea3ae3db434683138e107b
callmetaufeeq <callmetaufeeq@gmail.com>
2023-01-28 11:58:39 +0530

    ICICI bank

---
+++ src/com/tw/taufeeq/ICICI.java
@@ -0,0 +1,35 @@
+package com.tw.taufeeq;
+
+import com.tw.faizan.AbstractionTest;
+
+public class ICICI extends AbstractionTest {
+
+	public ICICI(String name, String add) {
+		super(name, add);
+	}
+
+	@Override
+	public String checkBalance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String withrawAmount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String changePin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String depositAmount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
