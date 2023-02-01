2e5413e6058ab505a7c4312112128de0082a3c08
sohel3010 <shaikhsohel3010@gmail.com>
2023-02-01 12:05:17 +0530

    added one laptop information

--- src/com/tw/collectionP/ArrayListTest.java
+++ src/com/tw/collectionP/ArrayListTest.java
@@ -10,12 +10,14 @@ public class ArrayListTest {
 		Laptop l2=new Laptop(2, "HP", "002", 11000, "i7");
 		Laptop l3=new Laptop(3, "Dell", "003", 110001, "i7");
 		Laptop l4=new Laptop(4, "lenovo", "004", 110021, "i7");
+		Laptop l5=new Laptop(5, "Lenovo", "004", 43800, "i5");
 		
 		List<Laptop> list=new ArrayList<>();
 		list.add(l1);
 		list.add(l2);
 		list.add(l3);
 		list.add(l4);
+		list.add(l5);
 		
 		for (Laptop obj : list) {
 			System.out.println(obj.toString());
