f1493558c296f898be8dd4f00d9245d10e30db18
callmetaufeeq <callmetaufeeq@gmail.com>
2023-02-08 19:39:39 +0530

    Taufeeq did practice on LinkedList

---
+++ src/com/tw/taufeeq/LinkedPractice1.java
@@ -0,0 +1,27 @@
+package com.tw.taufeeq;
+
+import java.util.LinkedList;
+
+public class LinkedPractice1 {
+
+	public static void main(String[] args) {
+		
+LinkedList<String> l=new LinkedList<>();
+				l.add("Taufeeq");
+				l.add("Iphone 13pro");
+				l.add("kab le raha hai");
+				System.out.println(l);
+				 
+				for (String obj : l) {
+					System.out.println(obj);
+					
+				} {
+					
+				}
+				}
+			
+
+
+	}
+
+
