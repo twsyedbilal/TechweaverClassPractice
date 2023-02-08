db2c43fd6d2e27d43fdf9462dcddeb9dbbebbeb7
callmetaufeeq <callmetaufeeq@gmail.com>
2023-02-08 19:26:28 +0530

    Taufeeq did practice on LinkedList

---
+++ src/com/tw/taufeeq/LinkedListPractice.java
@@ -0,0 +1,23 @@
+package com.tw.taufeeq;
+
+import java.util.LinkedList;
+
+public class LinkedListPractice {
+
+	public static void main(String[] args) {
+		LinkedList<String> l = new LinkedList<>();
+		l.add("Taufeeq");
+		l.add("Iphone 13pro");
+		l.add("kab le raha hai");
+		System.out.println(l);
+		 
+		for (String obj : l) {
+			System.out.println(obj);
+			
+		} {
+			
+		}
+		}
+	}
+
+
