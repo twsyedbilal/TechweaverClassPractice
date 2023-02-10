8cc453bef1cb5b756f6ed9bc8eacb0d227e3ea8f
callmetaufeeq <callmetaufeeq@gmail.com>
2023-02-10 22:05:00 +0530

    Taufeeq did practice on HashSet

---
+++ src/com/tw/taufeeq/HashSetPractice.java
@@ -0,0 +1,22 @@
+package com.tw.taufeeq;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class HashSetPractice {
+	
+	Set<String> s=new HashSet<>();
+	void add() {
+		s.add("company");
+		s.add("Boss");
+		s.add("employees");
+		s.add("workers");
+		
+	}
+	public static void main(String[] args) {
+		HashSetPractice t=new HashSetPractice();
+		t.add();
+		System.out.println(t.s);
+	}
+}
