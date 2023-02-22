84ec5cf3dc978bb516ae76ef016239d9fdad81f6
syedf1102 <syedf1102@gmail.com>
2023-02-22 14:49:40 -0800

    practice of multithreading.

---
+++ src/com/tw/faizan/MainF2.java
@@ -0,0 +1,14 @@
+package com.tw.faizan;
+
+public class MainF2 {
+
+	public static void main(String[] args) {
+		MultithreadingF2 m = new MultithreadingF2();
+		m.start();
+
+		MultithreadingF2 m2 = new MultithreadingF2();
+		m2.start();
+
+	}
+
+}
